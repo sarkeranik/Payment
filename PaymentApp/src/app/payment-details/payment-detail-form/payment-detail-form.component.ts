60fc819978d240c34cb0e9ae7dfb216112be288c
anik-sarker <16-32339-2@student.aiub.edu>
2021-03-20 23:48:40 +0600

    bootstrap verions changed from 5 to 4.5, form validation handled, httpclient added

--- PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentDetailsService } from 'src/app/shared/payment-details.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-detail-form',
@@ -10,4 +11,13 @@ export class PaymentDetailFormComponent implements OnInit {
   constructor(public service: PaymentDetailsService) {}
 
   ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    this.service.postPaymentDetail().subscribe(
+      (res) => {},
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
