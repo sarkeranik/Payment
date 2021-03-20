deb7f984311e8955f9b9bff9168708ee609a83fa
anik-sarker <16-32339-2@student.aiub.edu>
2021-03-20 18:02:50 +0600

    Payment API Integrated

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
